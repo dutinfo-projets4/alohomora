13adbbe90b48efd3e36474c94ccc1f07224b61aa
Léo Bergerot <leo.bergerot@etudiant.univ-reims.fr>
2018-02-18 17:15:45 +0100

    ajout du bouton everything

--- src/main/java/alohomora/ControllerInterface.java
+++ src/main/java/alohomora/ControllerInterface.java
@@ -1,5 +1,35 @@
 package alohomora;
 
+import javafx.css.PseudoClass;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.input.MouseEvent;
+
+
+
+
 public class ControllerInterface {
+    private boolean allElementIsActive = true;
+    @FXML
+    private HBox allElements;
+
+    @FXML
+    public void initialize() {
+
+    }
+    @FXML
+    public void onClickAllElement(MouseEvent e){
+
+
+        if(this.allElementIsActive) {
+            this.allElements.pseudoClassStateChanged(PseudoClass.getPseudoClass("pressed"), true);
+            this.allElementIsActive = false;
+        }
+        else {
+            this.allElements.pseudoClassStateChanged(PseudoClass.getPseudoClass("pressed"), false);
+            this.allElementIsActive = true;
+        }
+
+    }
 
 }
