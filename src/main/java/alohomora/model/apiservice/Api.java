4cfa38c451c0a0253551c520c1448c18641e742a
Nathan JANCZEWSKI <nathan.janczewski@etudiant.univ-reims.fr>
2018-03-18 13:18:43 +0100

    LICENSE + README

--- src/main/java/alohomora/model/apiservice/Api.java
+++ src/main/java/alohomora/model/apiservice/Api.java
@@ -3,7 +3,24 @@ package alohomora.model.apiservice;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-
+/**
+ * Alohomora Password Manager
+ * Copyright (C) 2018 Team Alohomora
+ * Léo BERGEROT, Sylvain COMBRAQUE, Sarah LAMOTTE, Nathan JANCZEWSKI
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 public class Api {
 	private Retrofit retrofit;
 	private AlohomoraService alohomoraService;
